74cfc53b3d8b0a63da2cd88d9651159a3dfef72a
Tiffany M <tiffanym@Tiffany.local>
2023-02-01 21:45:14 +0800

    Second Commit

---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const HomePage = () => {
+    return(
+        <div class="nav-container">
+            <button class="nav-button">§¢§£§¤</button>
+            <button class="nav-button">§Û§Õ§Ú§æ§Û§à?§Ú§Ö§æ</button>
+            <button class="nav-button">Menu</button>
+        </div>
+    );
+}
+
+export default HomePage;
